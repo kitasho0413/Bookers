2c2ae332f43c23a50ba836350d7890ac31c1d3f4
EC2 Default User <ec2-user@ip-172-31-4-109.ec2.internal>
2022-02-07 04:24:07 +0000

    [Update]格ページのレイアウト完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books/:id'
+    redirect_to book_path(book.id)
   end
   
   def show
@@ -16,6 +16,12 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
   
   def destroy
